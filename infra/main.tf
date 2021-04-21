91b3217e8ac2f5b9c1b065f3a4588d783239e0b7
Jeffersonn Lucas <jeffersonnlucas@gmail.com>
2021-04-21 18:21:07 -0300

    fix: remove module

--- infra/main.tf
+++ infra/main.tf
@@ -11,21 +11,33 @@ data "aws_caller_identity" "current" {
 
 module "lambda" {
   source        = "terraform-aws-modules/lambda/aws"
-  function_name = "my-lambda-existing-package-local"
-  description   = "My awesome lambda function"
+  function_name = "my-lambda-scheduled"
+  description   = "My lambda function scheduled"
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
+  publish = true
 
   source_path = "../src/lambda/python-lambda-scheduled"
 
+  allowed_triggers = {
+    ScheduleLambdaRule = {
+      principal = "events.amazonaws.com"
+      source_arn = aws_cloudwatch_event_rule.schedule.arn
+    }
+  }
+
   tags = {
-    Name = "my-lambda-existing-package-local"
+    Name = "my-lambda-scheduled"
   }
 }
 
-module "schedule_lambda" {
-  source              = "./modules/schedule"
-  enable              = true
-  lambda_arn          = module.lambda.this_lambda_function_arn
+resource "aws_cloudwatch_event_rule" "schedule" {
+  name          = "LambdaScheduleEvent"
+  description   = "Lambda Function Schedule Event Trigger"
   schedule_expression = "rate(1 minute)"
+}
+
+resource "aws_cloudwatch_event_target" "schedule_lambda_function" {
+  rule = aws_cloudwatch_event_rule.schedule.name
+  arn  = module.lambda.this_lambda_function_arn
 }
--- infra/variables.tf
+++
@@ -1,4 +0,0 @@
-
-# variable "schedule_expression" {
-#   description = "The scheduling expression. For example, cron (0 20 * *? *) Or fee (5 minutes). schedule_expression is mandatory if you have enabled this module."
-# }
